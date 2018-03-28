78e140892f8cba529f439a895be8df8ce4f4170c
Jason Smith <smithj6@qut.edu.au>
2018-03-28 19:34:27 +1000

    basic intent resolution

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-/*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
-
 var restify = require('restify');
 var builder = require('botbuilder');
-// var Store = require('./store');
 var botbuilder_azure = require("botbuilder-azure");
 
 // Setup Restify Server
@@ -12,7 +7,7 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -23,25 +18,12 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
 var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
-
 // Create your bot with a function to receive messages from the user
-// This default message handler is invoked if the user's utterance doesn't
-// match any intents handled by other dialogs.
-// var bot = new builder.UniversalBot(connector);
 var bot = new builder.UniversalBot(connector);
-// var bot = new builder.UniversalBot(connector, function (session, args) {
-//     session.send('You reached the default message handler. You said \'%s\'.', session.message.text);
-// });
 bot.set('storage', tableStorage);
 
 // Make sure you add code to validate these fields
@@ -54,128 +36,18 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+.matches('AssessmentDue', (session) => {
+    session.send('You reached Assessment due intent, you said \'%s\'.', session.message.text);
+})
+.matches('Help', (session) => {
+    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+})
+.matches('Cancel', (session) => {
+    session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+})
+.onDefault((session) => {
+    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+});
 
-// Add the recognizer to the bot
-bot.recognizer(recognizer); 
-var intents = new builder.IntentDialog({ recognizers: [recognizer] });
-
-bot.dialog('AssessmentDue', [
-    function (session) {
-        builder.Prompts.text(session, 'Looking up assessment dates...');
-    },
-    function (session, results) {
-        session.endDialog(`I found two assessment items for semester 1 2018`);
-    }
-]);
-
-// bot.dialog('SearchHotels', [
-//     function (session, args, next) {
-//         session.send('Welcome to the Hotels finder! We are analyzing your message: \'%s\'', session.message.text);
-
-//         // try extracting entities
-//         var cityEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
-//         var airportEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'AirportCode');
-//         if (cityEntity) {
-//             // city entity detected, continue to next step
-//             session.dialogData.searchType = 'city';
-//             next({ response: cityEntity.entity });
-//         } else if (airportEntity) {
-//             // airport entity detected, continue to next step
-//             session.dialogData.searchType = 'airport';
-//             next({ response: airportEntity.entity });
-//         } else {
-//             // no entities detected, ask user for a destination
-//             builder.Prompts.text(session, 'Please enter your destination');
-//         }
-//     },
-//     function (session, results) {
-//         var destination = results.response;
-
-//         var message = 'Looking for hotels';
-//         if (session.dialogData.searchType === 'airport') {
-//             message += ' near %s airport...';
-//         } else {
-//             message += ' in %s...';
-//         }
-
-//         session.send(message, destination);
-
-//         // Async search
-//         session.send('async search');
-//         // Store
-//         //     .searchHotels(destination)
-//         //     .then(function (hotels) {
-//         //         // args
-//         //         session.send('I found %d hotels:', hotels.length);
-
-//         //         var message = new builder.Message()
-//         //             .attachmentLayout(builder.AttachmentLayout.carousel)
-//         //             .attachments(hotels.map(hotelAsAttachment));
-
-//         //         session.send(message);
-
-//         //         // End
-//         //         session.endDialog();
-//         //     });
-//     }
-// ]).triggerAction({
-//     matches: 'SearchHotels',
-//     onInterrupted: function (session) {
-//         session.send('Please provide a destination');
-//     }
-// });
-
-// bot.dialog('ShowHotelsReviews', function (session, args) {
-//     // retrieve hotel name from matched entities
-//     var hotelEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Hotel');
-//     if (hotelEntity) {
-//         session.send('Looking for reviews of \'%s\'...', hotelEntity.entity);
-//         // Store.searchHotelReviews(hotelEntity.entity)
-//         //     .then(function (reviews) {
-//         //         var message = new builder.Message()
-//         //             .attachmentLayout(builder.AttachmentLayout.carousel)
-//         //             .attachments(reviews.map(reviewAsAttachment));
-//         //         session.endDialog(message);
-//         //     });
-//     }
-// }).triggerAction({
-//     matches: 'ShowHotelsReviews'
-// });
-
-// bot.dialog('greetings', [
-//     function (session) {
-//         builder.Prompts.text(session, 'Hi! What is your name?');
-//     },
-//     function (session, results) {
-//         session.endDialog(`Hello ${results.response}!`);
-//     }
-// ]);
-
-// bot.dialog('Help', function (session) {
-//     session.endDialog('Hi! Try asking me things like \'search hotels in Seattle\', \'search hotels near LAX airport\' or \'show me the reviews of The Bot Resort\'');
-// }).triggerAction({
-//     matches: 'Help'
-// });
-
-// // Helpers
-// function hotelAsAttachment(hotel) {
-//     return new builder.HeroCard()
-//         .title(hotel.name)
-//         .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
-//         .images([new builder.CardImage().url(hotel.image)])
-//         .buttons([
-//             new builder.CardAction()
-//                 .title('More details')
-//                 .type('openUrl')
-//                 .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
-//         ]);
-// }
-
-// function reviewAsAttachment(review) {
-//     return new builder.ThumbnailCard()
-//         .title(review.title)
-//         .text(review.text)
-//         .images([new builder.CardImage().url(review.image)]);
-// }
-
-bot.dialog('/', intents); 
+bot.dialog('/', intents);  
